ceedf7aa943ddf0cd4c84837757278914214ff0a
roylee0704 <roylee0704@gmail.com>
2021-02-07 21:17:13 +0700

    add access-control

---
+++ access-control/index.js
@@ -0,0 +1,15 @@
+import AccessControl from 'accesscontrol';
+const ac = new AccessControl();
+ac.grant('user')                    // define new or modify existing role. also takes an array.
+    .createOwn('video')             // equivalent to .createOwn('video', ['*'])
+    .deleteOwn('video')
+    .readAny('video')
+    .grant('admin')                   // switch to another role without breaking the chain
+    .extend('user')                 // inherit role capabilities. also takes an array
+    .updateAny('video', ['title'])  // explicitly defined attributes
+    .deleteAny('video');
+
+const permission = ac.can('user').createOwn('video');
+console.log(permission.granted);    // —> true
+console.log(permission.attributes); // —> ['*'] (all attributes)
+
