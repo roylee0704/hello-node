9c40c8abe6de75cc3910dca76d1082632133211b
roylee0704 <roylee0704@gmail.com>
2022-09-24 19:22:56 +0700

    add metadata generator

--- gen-thirdweb-metadata/metadata.js
+++ gen-thirdweb-metadata/metadata.js
@@ -1,17 +1,34 @@
 import fs from "fs";
-import path from "path";
-// read from animation dir
-// generate 10000 items into images
-const data = fs.readFileSync("atm/metadata.json", "utf8");
 
-const list = JSON.parse(data);
+// https://bafybeid7456yfoqrivmocrzafquwrmmbmflvmwntezmq4qzxbxv3dxqwsm.ipfs.nftstorage.link/silver.png
 
-for (let i = 0; i < list.length; i++) {
-  list[i].image =
-    "https://bafybeid56upesvkj34d4dycaku6v532m2i3saojoqsnoloiiz5zbx5q2le.ipfs.nftstorage.link/5496.mp4";
-  list[i].animation_url =
-    "https://bafybeid56upesvkj34d4dycaku6v532m2i3saojoqsnoloiiz5zbx5q2le.ipfs.nftstorage.link/5496.mp4";
+const iterator = [
+  { tier: "Silver", count: 6000 },
+  { tier: "Gold", count: 3000 },
+  { tier: "Diamond", count: 1000 },
+];
+
+const json = [];
+
+for (let i = 0; i < iterator.length; i++) {
+  console.log(iterator[i], "s");
+
+  for (let x = 0; x < iterator[i].count; x++) {
+    json.push({
+      name: "THE ELITE 101",
+      animation_url: `https://bafybeid7456yfoqrivmocrzafquwrmmbmflvmwntezmq4qzxbxv3dxqwsm.ipfs.nftstorage.link/${iterator[
+        i
+      ].tier.toLowerCase()}.mp4`,
+      image: `https://bafybeid7456yfoqrivmocrzafquwrmmbmflvmwntezmq4qzxbxv3dxqwsm.ipfs.nftstorage.link/${iterator[
+        i
+      ].tier.toLowerCase()}.png`,
+    });
+  }
+
+  //   list[i].image =
+  //     "https://bafybeid56upesvkj34d4dycaku6v532m2i3saojoqsnoloiiz5zbx5q2le.ipfs.nftstorage.link/5496.mp4";
+  //   list[i].animation_url =
+  //     "https://bafybeid56upesvkj34d4dycaku6v532m2i3saojoqsnoloiiz5zbx5q2le.ipfs.nftstorage.link/5496.mp4";
 }
 
-console.log(list[0].image);
-fs.writeFileSync("atm/metadata.json", JSON.stringify(list));
+fs.writeFileSync("atm/metadata.json", JSON.stringify(json));
